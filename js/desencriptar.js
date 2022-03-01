f9d3df762de2aaa131a97ede165f6fc012698e59
xDARBx <89049112+xDARBx@users.noreply.github.com>
2022-03-01 12:23:14 -0300

    Up challenge 1 Ver 0.1
    Challenge 1 semana 1 y 2
    Oracle + Alura

---
+++ js/desencriptar.js
@@ -0,0 +1,27 @@
+/* Reglas de desencriptación: 
+"enter" es convertido para "e" 
+"imes" es convertido para "i"
+"ai" es convertido para "a"
+"ober" es convertido para "o"
+"ufat" es convertido para "u"
+Solo letras minusculas
+No se permite acentuación de palabras   
+*/
+var botonDesencriptar = document.querySelector("#btn-desencriptar");
+
+botonDesencriptar.addEventListener("click", function(event) {
+    event.preventDefault();
+    
+    var mensaje = document.querySelector("#input-texto");
+    var valorMensaje = mensaje.value;
+    var campoDos = document.querySelector("#msg");
+    var decript = "";
+
+    decript= valorMensaje.replace(/ai/g, letras[0]).replace(/enter/g, letras[1]).
+    replace(/imes/g, letras[2]).replace(/ober/g, letras[3]).replace(/ufat/g, letras[4]);
+        
+    campoDos.value = decript;
+
+    limpiarCampoUno();
+
+});
---
+++ js/validar-texto.js
@@ -0,0 +1,4 @@
+                //Con el console, comprobamos que corresponda a la posición del filtroGeneral dando true
+                //console.log(filtroGeneral[j] == textoEntrada[i]);
+                textoValidado += textoEntrada[i];
+                console.log("Validado: " + textoValidado)
